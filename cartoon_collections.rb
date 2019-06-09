8bdb5b8d6b47d7bbaca54369b1d3825c697f186d
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 18:07:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ puts "#{number+1} #{dwarf}."
 end
 end 
 def summon_captain_planet(planeteer_calls)
-puts planeteer_calls.
+puts planeteer_calls.split.map(&:capitalize).join("! ")s
 end
 
 def long_planeteer_calls# code an argument here
