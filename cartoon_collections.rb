5aa7dfe80a378922106a390b454518d78f21cd37
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 18:04:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
 array.collect.each_with_index do |dwarf,number|
-puts "#{number} #{dwarf}."
+puts "#{number+1} #{dwarf}."
 end
 end 
 def summon_captain_planet# code an argument here
