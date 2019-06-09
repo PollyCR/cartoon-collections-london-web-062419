a274ade4e8fecae544e2719eaf7ced4221e8aaf1
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 18:22:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,13 +4,13 @@ puts "#{number+1} #{dwarf}."
 end
 end 
 def summon_captain_planet(array)
-  nu_array = []
-  i = 0 
-  while i < array.length 
-    nu_array << array[i].capitalize + "!"
-    i += 1 
+  calls = []
+  x = 0 
+  while x < array.length 
+    calls << array[x].capitalize + "!"
+    x += 1 
   end 
-  nu_array
+  calls
 end
 
 
