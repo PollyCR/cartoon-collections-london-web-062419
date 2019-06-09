b657602aa87c5ef715fa799699d916511cd62bf8
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 18:03:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)
 array.collect.each_with_index do |dwarf,number|
 puts "#{number} #{dwarf}."
 end
-
+end 
 def summon_captain_planet# code an argument here
   # Your code here
 end
@@ -15,3 +15,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
